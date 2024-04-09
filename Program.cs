91c14190b2c90bf2493a449d9a366e48a224b6ca
Andres Felipe Ospina <afelipe.ospina@gmail.com>
2024-04-08 23:19:07 -0500

    Pay Mode Presenter

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Supermarket_mvp.Views
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new PayModelView());
+            Application.Run(new PayModeView());
         }
     }
 }
--- Views/IPayModeView.cs
+++ Views/IPayModeView.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace Supermarket_mvp.Views
 {
-    internal interface IPayModelView
+    internal interface IPayModeView
     {
-        string PayModeID { get; set; }
+        string PayModeId { get; set; }
         string PayModeName { get; set; }
         string PayModeObservation { get; set; }
 
         string SearchValue { get; set; }
         bool IsEdit { get; set; }
         bool IsSuccessful { get; set; }
-        string Message {  get; set; }
+        string Message { get; set; }
 
         event EventHandler SearchEvent;
         event EventHandler AddNewEvent;
