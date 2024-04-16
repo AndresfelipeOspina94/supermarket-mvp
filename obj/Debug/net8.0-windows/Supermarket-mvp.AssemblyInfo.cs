97861a058b8e5e77fc62b2ec8221687585d7725e
Andres Felipe Ospina <afelipe.ospina@gmail.com>
2024-04-16 00:21:04 -0500

    Product Completo

--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ca5d586343a57bc88eeb3c9fb8f0719af682cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07dc18b17614a13c8713326957d11042505b8dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
