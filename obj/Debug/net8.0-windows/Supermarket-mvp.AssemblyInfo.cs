3d334abe803a9cc3f2fc907360ea92c4bc4249ad
Andres Felipe Ospina <afelipe.ospina@gmail.com>
2024-04-18 21:19:02 -0500

    CategoryViewe

--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba1989006c2e7f363081d8e16f64e656c8a9cfc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a77ccebd6b4f5b1b1e05252902163ae649b5413")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
